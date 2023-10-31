b65c096d3aaa6ded4918dab45333d1994e57091f
Chao-Han Tsai <milton0825@gmail.com>
2023-10-30 21:00:24 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-# Install homebrew
-echo "Checking if Homebrew is already installed..."; 
+echo "Setting up oh my zsh"
+if [ -d "${ZSH}" ]; then 
+  echo "Found existing oh my zsh installation at ${ZSH}"
+else
+  echo "Installing oh my zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+echo "Setting up homebrew"; 
 if test ! $(which brew); then
   echo "Installing Homebrew...";
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -13,6 +20,3 @@ fi
 # TODO: add this two lines to .zprofile, maybe this should live in dotfiles repo.
 # (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ctsai/.zprofile
 # eval "$(/opt/homebrew/bin/brew shellenv)"
-    
-brew update
-
