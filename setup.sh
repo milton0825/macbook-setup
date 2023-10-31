ea1293653ce099383eb405a6ef53126dfd056eb0
Chao-Han Tsai <milton0825@gmail.com>
2023-10-30 20:02:41 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
 
+# Install homebrew
+echo "Checking if Homebrew is already installed..."; 
+if test ! $(which brew); then
+  echo "Installing Homebrew...";
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew is already installed...";
+fi
