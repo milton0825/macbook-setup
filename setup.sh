b53ddc8d5d8898ed376d5dbeb7994b3483828d86
Chao-Han Tsai <milton0825@gmail.com>
2023-10-30 21:10:09 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,13 +14,15 @@ PATH="${PATH}:/opt/homebrew/bin"
 
 echo "Setting up homebrew"; 
 if test ! $(which brew); then
-  echo "Installing Homebrew...";
+  echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  echo "Homebrew is already installed...";
+  echo "Homebrew is already installed..."
 fi
 # TODO: add this two lines to .zprofile, maybe this should live in dotfiles repo.
 # (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ctsai/.zprofile
 # eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew update
+
+
